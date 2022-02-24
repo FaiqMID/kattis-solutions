11cfb0c0d3a643461b006713213bc2194d4c7698
FaiqMID <faiqm2003@gmail.com>
2022-02-24 13:47:24 +0700

    sorttwonumbers, timeloop

---
+++ Solutions/Stuck In A Time Loop/TimeLoop.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class TimeLoop {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    int input = scan.nextInt();
+    for (int i=1; i <= input; i++){
+      System.out.println(i + " Abracadabra");
+    }
+    scan.close();
+  }
+}
